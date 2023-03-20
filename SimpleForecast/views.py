0e62fb10555f1a36b12e6313fa528418b532138e
andre-v-sc <andrevscunha@gmail.com>
2023-03-20 16:17:12 -0400

    additonal GUI changes

--- SimpleForecast/views.py
+++ SimpleForecast/views.py
@@ -2,55 +2,50 @@ from django.shortcuts import render
 import json
 import requests
 
-# Home view function
+
+def get_category_data(aqi):
+    if aqi >= 0 and aqi <= 50:
+        return 'good', 'Air quality is considered satisfactory, and air pollution poses little or no risk.'
+    elif aqi >= 51 and aqi <= 100:
+        return 'moderate', 'Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.'
+    elif aqi >= 101 and aqi <= 150:
+        return 'usg', 'Members of sensitive groups may experience health effects. The general public is not likely to be affected.'
+    elif aqi >= 151 and aqi <= 200:
+        return 'unhealthy', 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.'
+    elif aqi >= 201 and aqi <= 300:
+        return 'veryunhealthy', 'Health alert: everyone may experience more serious health effects.'
+    elif aqi >= 301:
+        return 'hazardous', 'Health warnings of emergency conditions. The entire population is more likely to be affected.'
+
+    return None, None
+
+
 def home(request):
-    
-     #Check if the request method is POST, else use default zip
     if request.method == "POST":
         zip = request.POST['zip']
         api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zip + "&distance=5&API_KEY=2016116F-EA63-4ABC-ACA0-0EBB2C79EA16")
     else:
         api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=19087&distance=5&API_KEY=2016116F-EA63-4ABC-ACA0-0EBB2C79EA16")
-    
-    # Load API response and handle errors
+
     try:
         api = json.loads(api_request.content)
     except Exception as e:
         api = "Error..."
-    # Determine air quality category and descriptions based on API response
-    if len(api) > 0:
-        # Category-specific descriptions and colors
-        if api[0]['Category']['Name'] == "Good":
-            category_description = "(0 - 50) Air quality is satisfactory, and air pollution poses little or no risk."
-            category_color = "good"
-        elif api[0]['Category']['Name'] == "Moderate":
-            category_description = "(51 - 100) Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution."
-            category_color = "moderate"
-        elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
-            category_description = "(101 - 150) Members of sensitive groups may experience health effects. The general public is less likely to be affected."
-            category_color = "usg"
-        elif api[0]['Category']['Name'] == "Unhealthy":
-            category_description = "(151 - 200) Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects."
-            category_color = "unhealthy"
-        elif api[0]['Category']['Name'] == "Very Unhealthy":
-            category_description = "(201 - 300) Health alert: The risk of health effects is increased for everyone."
-            category_color = "veryunhealthy"
-        elif api[0]['Category']['Name'] == "Hazardous":
-            category_description = "(301 - higher) Health warning of emergency conditions: everyone is more likely to be affected."
-            category_color = "hazardous"
-    else:
-        # If no data is available for given zip
-        category_description = "No data available for the provided zip code."
-        category_color = "no-data"
 
-    # Render home.html with air quality data
+    if api and api != "Error...":
+        aqi = api[0]['AQI']
+        category_color, category_description = get_category_data(aqi)
+        print("AQI:", aqi, "Color:", category_color, "Description:", category_description)
+    else:
+        category_color = "no-data"
+        category_description = "No data available for the provided zip code."
+
     return render(request, 'home.html', {
-        'api' : api, 
-        'category_description': category_description, 
+        'api': api,
+        'category_description': category_description,
         'category_color': category_color
     })
 
-# About view function
+
 def about(request):
-    # Render about.html
     return render(request, 'about.html', {})
